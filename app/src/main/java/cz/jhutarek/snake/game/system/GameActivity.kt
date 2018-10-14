78795773b6cc08fa4f67d3b72ec13b77b0a0b84b
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-10-14 20:29:03 +0200

    Rename activity

--- app/src/main/java/cz/jhutarek/snake/game/system/GameActivity.kt
+++ app/src/main/java/cz/jhutarek/snake/game/system/GameActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cz.jhutarek.snake.R
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.game__game_activity)
     }
 }
