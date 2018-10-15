34039753a4e8acc26791589eb3802df9730835a4
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-10-15 17:07:36 +0200

    Rename variable

--- app/src/main/java/cz/jhutarek/snake/game/model/Snake.kt
+++ app/src/main/java/cz/jhutarek/snake/game/model/Snake.kt
@@ -1,32 +1,32 @@
 package cz.jhutarek.snake.game.model
 
 data class Snake(
-        val cells: List<Cell>,
-        private val direction: Direction,
-        private val applesToEat: Int = 0,
-        val applesEaten: Int = 0
+    val cells: List<Cell>,
+    private val direction: Direction,
+    private val applesDigesting: Int = 0,
+    val applesEaten: Int = 0
 ) {
     val head = cells.first()
     val tail = cells.drop(1)
 
     fun move(): Snake {
         val newHead = head.move(direction)
-        val newTail = if (applesToEat == 0) cells.dropLast(1) else cells
+        val newTail = if (applesDigesting == 0) cells.dropLast(1) else cells
 
         return copy(
-                cells = listOf(newHead) + newTail,
-                applesToEat = maxOf(0, applesToEat - 1)
+            cells = listOf(newHead) + newTail,
+            applesDigesting = maxOf(0, applesDigesting - 1)
         )
     }
 
     fun turn(newDirection: Direction) =
-            if (newDirection.isOpposite(direction)) this
-            else copy(direction = newDirection)
+        if (newDirection.isOpposite(direction)) this
+        else copy(direction = newDirection)
 
     fun eat(apples: Apples) =
-            if (!apples.cells.contains(head)) Pair(this, apples)
-            else Pair(
-                    this.copy(applesToEat = applesToEat + 1, applesEaten = applesEaten + 1),
-                    apples.copy(cells = apples.cells - head)
-            )
+        if (!apples.cells.contains(head)) Pair(this, apples)
+        else Pair(
+            this.copy(applesDigesting = applesDigesting + 1, applesEaten = applesEaten + 1),
+            apples.copy(cells = apples.cells - head)
+        )
 }
