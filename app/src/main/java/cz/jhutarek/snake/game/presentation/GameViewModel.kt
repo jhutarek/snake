0b6e556cfb6b7d99619927ce2552828891787581
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-10-21 13:03:04 +0200

    Implement game view model

--- app/src/main/java/cz/jhutarek/snake/game/presentation/GameViewModel.kt
+++ app/src/main/java/cz/jhutarek/snake/game/presentation/GameViewModel.kt
@@ -1,17 +1,21 @@
 package cz.jhutarek.snake.game.presentation
 
-import cz.jhutarek.snake.game.domain.Game
 import cz.jhutarek.snake.game.model.Cell
 import cz.jhutarek.snake.game.model.Dimensions
 import cz.jhutarek.snake.game.model.Direction.*
 import cz.jhutarek.snake.game.model.Point
+import cz.jhutarek.snake.game.model.State.*
 import cz.jhutarek.snake.game.model.abs
+import cz.jhutarek.snake.game.model.State as GameState
 
 typealias GameViewModelListener = (GameViewModel.State) -> Unit
 
 class GameViewModel(
-    private val game: Game
+    private val game: cz.jhutarek.snake.game.domain.Game
 ) {
+    init {
+        game.listener = { update(it) }
+    }
 
     data class State(
         val introVisible: Boolean,
@@ -19,11 +23,29 @@ class GameViewModel(
         val overVisible: Boolean,
         val field: Dimensions?,
         val snake: List<Cell>?,
-        val apples: List<Cell>?,
+        val apples: Set<Cell>?,
         val score: Int?
     )
 
+    private var state = State(
+        introVisible = true,
+        gameVisible = false,
+        overVisible = false,
+        field = null,
+        snake = null,
+        apples = null,
+        score = null
+    )
+        set(value) {
+            field = value
+            listener?.invoke(field)
+        }
+
     var listener: GameViewModelListener? = null
+        set(value) {
+            field = value
+            field?.invoke(state)
+        }
 
     fun reset() {
         game.reset()
@@ -46,5 +68,37 @@ class GameViewModel(
                 else -> UP
             }
         }.let { game.direction = it }
+    }
+
+    private fun update(gameState: GameState) {
+        state = when (gameState) {
+            is Waiting -> State(
+                introVisible = true,
+                gameVisible = false,
+                overVisible = false,
+                field = null,
+                snake = null,
+                apples = null,
+                score = null
+            )
+            is Running -> State(
+                introVisible = false,
+                gameVisible = true,
+                overVisible = false,
+                field = gameState.field,
+                snake = gameState.snake.cells,
+                apples = gameState.apples.cells,
+                score = gameState.score
+            )
+            is Over -> State(
+                introVisible = false,
+                gameVisible = false,
+                overVisible = true,
+                field = null,
+                snake = null,
+                apples = null,
+                score = gameState.score
+            )
+        }
     }
 }
--- app/src/test/java/cz/jhutarek/snake/game/presentation/GameViewModelTest.kt
+++ app/src/test/java/cz/jhutarek/snake/game/presentation/GameViewModelTest.kt
@@ -1,20 +1,27 @@
 package cz.jhutarek.snake.game.presentation
 
 import cz.jhutarek.snake.game.domain.Game
+import cz.jhutarek.snake.game.domain.GameListener
+import cz.jhutarek.snake.game.model.*
 import cz.jhutarek.snake.game.model.Direction.*
-import cz.jhutarek.snake.game.model.Point
 import cz.jhutarek.snake.game.testinfrastructure.CustomStringSpec
 import io.kotlintest.data.forall
 import io.kotlintest.tables.row
-import io.mockk.clearMocks
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
 
 internal class GameViewModelTest : CustomStringSpec({
 
-    val game = mockk<Game>(relaxUnitFun = true)
+    val gameListenerSlot = slot<GameListener>()
+    val game = mockk<Game>(relaxUnitFun = true) {
+        every { listener = capture(gameListenerSlot) } returns Unit
+    }
+    val listener = mockk<GameViewModelListener> {
+        every { this@mockk(any()) } returns Unit
+    }
 
-    val viewModel = GameViewModel(game)
+    val viewModel = GameViewModel(game).apply {
+        this.listener = listener
+    }
 
     "reset should reset the game" {
         viewModel.reset()
@@ -49,6 +56,83 @@ internal class GameViewModelTest : CustomStringSpec({
             viewModel.swipe(from, to)
 
             verify { game.direction = expectedDirection }
+        }
+    }
+
+    "view model should notify listener with default state when added after construction" {
+        verify {
+            listener(
+                GameViewModel.State(
+                    introVisible = true,
+                    gameVisible = false,
+                    overVisible = false,
+                    field = null,
+                    snake = null,
+                    apples = null,
+                    score = null
+                )
+            )
+        }
+    }
+
+    "view model should register game listener" {
+        verify { game.listener = any() }
+    }
+
+    "view model should map game state to view model state" {
+        val field = Dimensions(15, 16)
+        val snake = Snake(listOf(Cell(1, 1), Cell(2, 1), Cell(3, 1)), LEFT, 5, 1)
+        val apples = Apples(setOf(Cell(0, 0)), field)
+        val runningState = State.Running(
+            field = field,
+            snake = snake,
+            apples = apples
+        )
+        val overState = State.Over(456)
+
+        forall(
+            row(
+                GameViewModel.State(
+                    introVisible = true,
+                    gameVisible = false,
+                    overVisible = false,
+                    field = null,
+                    snake = null,
+                    apples = null,
+                    score = null
+                ),
+                State.Waiting
+            ),
+            row(
+                GameViewModel.State(
+                    introVisible = false,
+                    gameVisible = true,
+                    overVisible = false,
+                    field = field,
+                    snake = snake.cells,
+                    apples = apples.cells,
+                    score = runningState.score
+                ),
+                runningState
+            ),
+            row(
+                GameViewModel.State(
+                    introVisible = false,
+                    gameVisible = false,
+                    overVisible = true,
+                    field = null,
+                    snake = null,
+                    apples = null,
+                    score = overState.score
+                ),
+                overState
+            )
+        ) { expectedViewModelState, gameState ->
+            clearMocks(listener, answers = false)
+
+            gameListenerSlot.captured(gameState)
+
+            verify { listener(expectedViewModelState) }
         }
     }
 })
